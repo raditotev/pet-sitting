d53e06f962d1c7b3e4450f2b992dd6908fe02935
raditotev <raditotev@gmail.com>
2015-11-17 10:23:30 +0000

    Added login and register to Static Pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
   def index
   end
+
+  def register
+    @sitter = Sitter.new
+    @owner = Owner.new
+  end
+
+  def login
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root 'static_pages#index'
+  get 'register' => 'static_pages#register'
+  get 'login' => 'static_pages#login'
 
   resources :sitters, only: [:new, :create, :show]
   resources :owners, only: [:new, :create, :show]
