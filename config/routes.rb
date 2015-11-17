bdc032af6163003478099c57846d253254795101
raditotev <raditotev@gmail.com>
2015-11-17 12:14:11 +0000

    Changed Static Pages to Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#index'
-  get 'register' => 'static_pages#register'
-  get 'login' => 'static_pages#login'
-  post 'login' => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
+  root 'pages#index'
+  get 'register'      => 'pages#register'
+  get 'login'          => 'pages#login'
+  post 'login'        => 'sessions#create'
+  delete 'logout'   => 'sessions#destroy'
 
   resources :sitters, only: [:create, :show]
   resources :owners, only: [:create, :show]
