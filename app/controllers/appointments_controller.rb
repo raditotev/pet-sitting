e96b2c7149e794c7d6665696f9ff891986567ead
raditotev <raditotev@gmail.com>
2015-11-18 11:13:39 +0000

    Sitter can take on a job - appointments#update

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -12,13 +12,16 @@ class AppointmentsController < ApplicationController
   end
 
   def update
-
+    appointment = Appointment.find(params[:id])
+    appointment.update_attribute(:sitter_id, appointment_params[:sitter_id])
+    #add message
+    redirect_to appointments_path
   end
 
   private
 
   def appointment_params
-    params.require(:appointment).permit(:owner_id, :date)
+    params.require(:appointment).permit(:owner_id, :sitter_id, :date)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
   root 'pages#index'
-  get 'register'         => 'pages#register'
-  get 'login'             => 'pages#login'
-  post 'login'            => 'sessions#create'
-  delete 'logout'       => 'sessions#destroy'
-  get 'profile'            => 'pages#profile'
-  get 'board'             => 'pages#board'
-  get 'appointments' => 'pages#appointments'
-  post 'appointments' => 'appointments#create'
+  get 'register'               => 'pages#register'
+  get 'login'                   => 'pages#login'
+  post 'login'                  => 'sessions#create'
+  delete 'logout'             => 'sessions#destroy'
+  get 'profile'                 => 'pages#profile'
+  get 'board'                  => 'pages#board'
+  get 'appointments'      => 'pages#appointments'
+  post 'appointments'     => 'appointments#create'
+  patch 'appointments' => 'appointments#update'
 
   resources :sitters, only: [:create]
   resources :owners, only: [:create]
